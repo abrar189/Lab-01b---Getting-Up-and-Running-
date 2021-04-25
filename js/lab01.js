ba196a67c17d5d8ce1de55c5b3b05f4957c7ed10
abrar189 <algourabrar@gmail.com>
2021-04-25 14:58:07 +0300

    done

---
+++ js/lab01.js
@@ -0,0 +1,12 @@
+let Username = prompt('please enter your name');
+alert("welcome " + Username);
+console.log(Username)
+let age = prompt('please enter your age?');
+console.log(age);
+let love = prompt('do you love food?');
+alert("don't worry we all love food")
+console.log(love);
+let Weight = prompt('please enter your weight?');
+console.log(Weight);
+
+
